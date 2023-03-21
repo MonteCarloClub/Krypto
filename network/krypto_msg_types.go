38b2f6e7118e5b3c4eebdb58e520f4ce913001c1
赵云潇 <zhao.yunxiao@outlook.com>
2023-03-21 21:32:38 +0800

    string to byte

--- network/krypto_msg_types.go
+++ network/krypto_msg_types.go
@@ -1,26 +1,26 @@
 package network
 
 type EncryptMsg struct {
-	PlainText string `json:"plain_text"`
+	PlainText []byte `json:"plain_text"`
 	Pub       []byte `json:"pub"`
 }
 
 type DecryptMsg struct {
-	CipherText string `json:"cipher_text"`
+	CipherText []byte `json:"cipher_text"`
 	Priv       []byte `json:"priv"`
 }
 
 type SignMsg struct {
-	PlainText string `json:"plain_text"`
+	PlainText []byte `json:"plain_text"`
 	Priv      []byte `json:"priv"`
 }
 
 type VerifyMsg struct {
-	PlainText  string `json:"plain_text"`
-	SignResult string `json:"sign_result"`
+	PlainText  []byte `json:"plain_text"`
+	SignResult []byte `json:"sign_result"`
 	Pub        []byte `json:"pub"`
 }
 
 type HashMsg struct {
-	PlainText string `json:"plain_text"`
+	PlainText []byte `json:"plain_text"`
 }
